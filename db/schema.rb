7131d7f08f319c3a574e2e62ab597b14c58f7161
maysix <alip.aswalid@gmail.com>
2015-09-18 06:51:48 +0800

    created task scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917143250) do
+ActiveRecord::Schema.define(version: 20150917224639) do
+
+  create_table "tasks", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
